d9887dd52438f719d7508e54e1b50eb81b6f0279
Adrian.Brand <brandadrian@gmail.com>
2019-11-08 13:59:56 +0100

    Komponenten hinzugefügt

--- kaese/src/app/app-routing.module.ts
+++ kaese/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StravaComponent } from './strava/strava.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'strava', component: StravaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
