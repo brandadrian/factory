cc31f9c7bb7e7cb9a104e8312279be248b473894
Adrian.Brand <brandadrian@gmail.com>
2019-11-08 15:39:51 +0100

    SudokuSolver in Menu

--- kaese/src/app/app-routing.module.ts
+++ kaese/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { StravaComponent } from './strava/strava.component';
+import { SudokuSolverComponent } from './sudoku-solver/sudoku-solver.component';
 
 const routes: Routes = [
-  {path: 'strava', component: StravaComponent}
+  {path: 'strava', component: StravaComponent},
+  {path: 'sudokuSolver', component: SudokuSolverComponent}
 ];
 
 @NgModule({
