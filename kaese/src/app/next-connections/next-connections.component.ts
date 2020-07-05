18036fe9dec661c8ea5e2be6e0ee793313380f96
Adrian Brand <adrian.brand@gmx.ch>
2020-07-05 23:11:48 +0200

    next-connections überarbeitet

--- kaese/src/app/next-connections/next-connections.component.ts
+++ kaese/src/app/next-connections/next-connections.component.ts
@@ -11,14 +11,20 @@ export class NextConnectionsComponent implements OnInit {
   public secondsTo: number;
   public minutesToStGallen: number;
   public minutesToZurich: number;
+  public nextToZurich: number;
+  public nextToStGallen: number;
   private timeTableToStGallen = [4, 26, 34, 56];
+  private timeTableToZurich = [4, 26, 34, 56];
 
   constructor() { 
     setInterval(() => {
       this.now = new Date();
+      const minutesNow = this.now.getMinutes();
       this.secondsTo = this.getSeconds();
-      this.minutesToStGallen = this.getMinutes(56);
-      this.minutesToZurich = this.getMinutes(26);
+      this.nextToStGallen = this.getNextTime(minutesNow, this.timeTableToStGallen);
+      this.nextToZurich = this.getNextTime(minutesNow, this.timeTableToZurich);
+      this.minutesToStGallen = this.getMinutes(this.nextToStGallen);
+      this.minutesToZurich = this.getMinutes(this.nextToZurich);
     }, 1);
   }
 
@@ -27,11 +33,26 @@ export class NextConnectionsComponent implements OnInit {
 
   getMinutes(timeTo) {
     const minutesNow = this.now.getMinutes();
-    return ((timeTo - minutesNow) < 0) ? 60 - minutesNow + timeTo: timeTo - minutesNow; 
+    return ((timeTo - minutesNow) < 0) ? 60 - minutesNow + timeTo : timeTo - minutesNow;
   }
 
   getSeconds() {
     const secondsNow = this.now.getSeconds();
-    return 60 - secondsNow == 60 ? 0 : 60 - secondsNow ;
+    return 60 - secondsNow === 60 ? 0 : 60 - secondsNow ;
+  }
+
+  getNextTime(minutesNow, timeTable)
+  {
+
+    for (let item in timeTable)
+    {
+      var nextItem = Number(item) + 1;
+      if (timeTable[item] < minutesNow && timeTable[nextItem] > minutesNow)
+      {
+        return timeTable[nextItem];
+      }
+      
+      return timeTable['0'];
+    }
   }
 }
