3ff418ef7c6f98af585e09e11f6d821a38a11fe8
brandadr <brandadrian@gmail.com>
2020-11-17 12:11:22 +0100

    mongoDbAccess erstellt

---
+++ factory_python_backend_v2/mongoDbAccess.py
@@ -0,0 +1,34 @@
+import json
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+class mongoDbAccess:
+
+    #databaseName e.g "my-mongo-db"
+    #collectionName e.g. "my-mongo-db-collection"
+    #connectionString e.g. "mongodb+srv://root:root@cluster0.srbj2.mongodb.net/mongo-db-name?retryWrites=true&w=majority"
+    def __init__(self, connectionString, collectionName, databaseName):
+        client = MongoClient(connectionString)
+        db = client[databaseName]
+        self.collection = db[collectionName]
+   
+    def save(self, document):
+        self.collection.insert_one(document)
+    
+    def readAll(self):
+        documents = self.collection.find()
+        response = []
+        for document in documents:
+            document['_id'] = str(document['_id'])
+            response.append(document)
+        return json.dumps(response)
+    
+    def readById(self, id):
+        document = self.collection.find_one({"_id": ObjectId(id)})
+        #document['_id'] = str(document['_id'])
+        return json.dumps(str(document))
+
+    def delete(self, id):
+        self.collection.delete_one({"_id": ObjectId(id)})  
+
+
--- factory_python_backend_v2/runningEventsApi.py
+++ factory_python_backend_v2/runningEventsApi.py
@@ -1,34 +1,36 @@
-import os
 import json
-from pymongo import MongoClient
 from flask import Flask, request
-from bson.objectid import ObjectId
-from dataAccess import dataAccess
+from mongoDbAccess import mongoDbAccess
 
 app = Flask(__name__)
 
 @app.route('/running-event')
 def get():
-    documents = dataAccess.readAll()
-    response = []
-    for document in documents:
-        document['_id'] = str(document['_id'])
-        response.append(document)
-    return json.dumps(response)
+    return runningEventDbAccess.readAll()
+
+@app.route('/running-event/<id>')
+def getById(id):
+    return runningEventDbAccess.readById(id)
 
 @app.route('/running-event', methods=['POST'])
 def post():
-    dataAccess.save(req_data)
+    request_data = request.get_json()
+    runningEventDbAccess.save(request_data)
     return ('', 200)
 
 @app.route('/running-event/<id>', methods=['DELETE'])
 def delete(id):
-    dataAccess.delete(id)
+    runningEventDbAccess.delete(id)
     return ('', 200)
 
 if __name__ == '__main__':
     with open('config.json') as data_file:
         data = json.load(data_file)
         port = data["flask_port"]
+        connectionString = data["mongo_db_connection"]
+        collectionRunningEvent = data["mongo_db_collection_running_event"]
+        dbName = data["mongo_db_name"]
+    
+    runningEventDbAccess = mongoDbAccess(connectionString, collectionRunningEvent, dbName)
     app.run('', port)
-
+    
